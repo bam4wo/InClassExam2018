ce7d6361d9b1f130bcad789cbaa81bfe3268cdb7
Bam <wubetty0727@gmail.com>
2018-12-12 01:55:43 +0800

    final

--- src/com/exam/Order.java
+++ src/com/exam/Order.java
@@ -1,11 +1,15 @@
 package com.exam;
 
+import java.util.ArrayList;
+
 public class Order {
 	public Order() {
-		
+	
 	}
 	
+	ArrayList machine = new ArrayList();
 	public void add(Ticket ticket) {
+		machine.add(ticket);
 		ticket.print();
 	}
 	
