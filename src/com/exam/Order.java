78b07259e3d5e624abd902813e12357e241840e4
Bam <wubetty0727@gmail.com>
2018-12-11 21:10:43 +0800

    modify

--- src/com/exam/Order.java
+++ src/com/exam/Order.java
@@ -1,5 +1,24 @@
 package com.exam;
 
 public class Order {
-
+	public Order() {
+		
+	}
+	
+	public void add(Ticket ticket) {
+		//System.out.println(t.met()+t.num()+t.numm()+t.allPrice());
+		/*t.met();
+		t.num();
+		t.numm();*/
+		/*Ticket tic = new Ticket();
+		tic.print();
+		tic = new Ticket();
+		tic.print();*/
+		ticket.print();
+	}
+	
+	public void print() {
+		Ticket a = new Ticket();
+		//System.out.println(a.met()+a.num()+a.numm()+a.allPrice());
+	}
 }
--- src/com/exam/Ticket.java
+++ src/com/exam/Ticket.java
@@ -1,5 +1,46 @@
 package com.exam;
 
 public class Ticket {
+	String origin;
+	String destination;
+	int price;
+	int quantity;
+	
+	public Ticket(String origin, String destination, int price, int quantity) {
+		this.origin = origin;
+		this.destination = destination;
+		this.price = price;
+		this.quantity = quantity;
+	}
+	
+	public Ticket() {
+		
+	}
+	
+	public void  print(){
+		System.out.println(origin+"\t"+destination+"\t"+price+"\t"+quantity+"\t"+price*quantity);
+		
+	}
+    
+	
+	/*public String met(){
+		String one = origin + destination;
+		return one;
+	}
+	
+	public int num() {
+		int number = price;
+		return number;
+	}
+	
+	public int numm() {
+		int number2 = quantity;
+		return number2;
+
+	}*/
+	public int allPrice() {
+		int allPrice = price*quantity;
+		return allPrice;
+	}
 
 }
